b8fe246b6bddb8d6c18088274c0712f0cfa65303
SSOsh <53353714+SSOsh@users.noreply.github.com>
2021-04-11 17:15:36 +0900

    error
    아직 에러상태. mysql의 포트문제 아직 해결안됨

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule GenericFinderServer.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
       {:crawly, "~> 0.13.0"},
       {:floki, "~> 0.26.0"}
     ]
