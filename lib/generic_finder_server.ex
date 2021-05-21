6bb9ace0ddbb9aa78e4a259a6fcdb6f4567684c4
GFrog <cys7818@naver.com>
2021-05-21 19:05:51 +0900

    손실된 부분 복구

--- lib/generic_finder_server.ex
+++ lib/generic_finder_server.ex
@@ -6,4 +6,37 @@ defmodule GenericFinderServer do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  @impl Crawly.Spider
+  def base_url(), do: ""
+
+  def medicine_url(), do: "https://nedrug.mfds.go.kr/pbp/CCBBB01/getItemDetail?itemSeq="
+
+  def businessNum_url(), do: "https://bizno.net/article/"
+
+  @impl Crawly.Spider
+  def init() do
+    [
+      start_urls: [
+        "https://www.homebase.co.uk/our-range/tools"
+      ]
+    ]
+  end
+
+  def get_med_info(itemSeq) do
+    response = Crawly.fetch(medicine_url()<>itemSeq)
+    {:ok, document} = Floki.parse_document(response.body)
+    (document |> Floki.find(".s-dr_table.dr_table_type1 tr") |> Floki.text()) #기본 정보
+    <> "\n" <> (document |> Floki.find(".note") |> Floki.text()) #유효성분
+    <> "\n" <> (document |> Floki.find("#_ee_doc") |> Floki.text()) #효능효과
+    <> "\n" <> (document |> Floki.find("#_ud_doc") |> Floki.text()) #용법용량
+    <> "\n" <> (document |> Floki.find("#_nb_doc") |> Floki.text()) #사용상주의사항
+  end
+
+  def get_pharm_info(businessNum) do
+    response = Crawly.fetch(businessNum_url() <> businessNum)
+    {:ok, document} = Floki.parse_document(response.body)
+    (document |> Floki.find(".table_guide01 tr") |> Floki.text()) #가게 명
+    <> "\n" <> (document |> Floki.find("tr") |> Floki.text() |> String.split("회사주소", parts: 2)
+                |> List.last() |> String.split("결산월") |> List.first())
+  end
 end
--- lib/generic_finder_server_web/controllers/page_controller.ex
+++ lib/generic_finder_server_web/controllers/page_controller.ex
@@ -4,4 +4,25 @@ defmodule GenericFinderServerWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def query(conn, _params) do
+    conn
+    |> assign(:result, GenericFinderServer.DbModel.Queries.selectOne)
+    |> render("query.html")
+  end
+
+  def api(conn, _params) do
+    conn
+    |> assign(:result, GenericFinderServer.Api.Call.callApi)
+    |> render("api.html")
+  end
+
+  def medicineDetail(conn, %{"medicineCode" => medicineCode}) do
+    #detail = GenericFinderServer.get_med_info(medicineCode)
+    json conn, GenericFinderServer.get_med_info(medicineCode)
+  end
+
+  def pharmacy(conn, %{"businessNum" => businessNum}) do
+    json conn, GenericFinderServer.get_pharm_info(businessNum)
+  end
 end
